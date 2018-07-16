3ad1b275329cc06b0e91525066cdeb813d591b84
Joseph Isbell <shadowwolf347@comcast.net>
2018-07-16 15:11:44 -0600

    updated math function code

--- challenge.js
+++ challenge.js
@@ -2,18 +2,19 @@
 // Math Challenge for JavaScript
 
 //takes two numbers at random between 0-10, adds them together and returns the result
-function randomNumber(number1, number2) {
-	return Math.floor((Math.random(number1)*10) + (Math.random(number2)*10));
+function diceRoll(number1, number2) {
+	dice = Math.floor((Math.random(number1)*6) + (Math.random(number2)*6));
+	return `You roll the dice and get a ${dice}!`;
 }
 
-console.log(randomNumber());
+console.log(diceRoll());
 
 // String Challenge for Javascript
 
-let animalName = 'Dog';
-
+let animalName = "dog"
 //Takes animalName from the global scope and inserts it into the parameter of the favoriteAnimal function
 const favoriteAnimal = (animal) => {
+
 	//returns a different string depending on the animalName passed into the function
 	if(animal.toLowerCase() === 'cat') {
 		return `In Japanese, ${animal}s say "にゃ"!`;
@@ -35,7 +36,7 @@ console.log(favoriteAnimal(animalName));
 const dylansShows = ['Star Trek', 'Office Space', 'Star Wars', 'The Simpsons'];
 const dylansFavorites = [];
 const moviesNotWorthSeeing = [];
-
+// Loops through each item in the dylansShows array and pushes it into either the dylansFavorites or moviesNotWorthSeeing array depending on if the string "Wars" is present.
 function shows() {
 	for(let i = 0; i < dylansShows.length; i++) {
 		if(dylansShows[i].includes('Wars')) {
@@ -44,7 +45,7 @@ function shows() {
 			dylansFavorites.push(dylansShows[i]);
 		}
 	}
-	return `Some really cool shows to watch are: ${dylansFavorites.join(', ')}. Movies you want to avoid at all costs are: ${moviesNotWorthSeeing.join('')}.`;
+	return `Some really cool shows to watch are: ${dylansFavorites.join(', ')}. Movies you want to avoid at all costs are: ${moviesNotWorthSeeing.join(', ')}.`;
 }
 
 console.log(shows());
