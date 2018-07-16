b61d8de4df162d20f60da81476839e1c1fd848d0
Joseph Isbell <shadowwolf347@comcast.net>
2018-07-16 16:36:30 -0600

    Added array challenge to php

---
+++ challenge.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * @params
+ * @return
+ **/
+// Math Dhallenge in PHP
+
+//Takes two random numbers between 1-6 and adds them together then returns the sum;
+function diceRoll() {
+	$dice = rand(1,6) + rand(1,6);
+	return "You rolled a $dice!";
+};
+
+echo diceRoll();
+
+// String Challenge in PHP
+function animals($animalName) {
+	if($animalName === "Rabbit")
+		return "$animalName is written ウサギ in Japanese.";
+	elseif($animalName === "Horse")
+		return "$animalName is written 馬 in Japanese.";
+	elseif($animalName === "Cat")
+		return "$animalName is written 猫 in Japanese.";
+	else
+		return "Your choice is highly illogical";
+};
+
+echo animals("Rabbit");
+
+// Array Challenge in PHP
+function shows() {
+	$dylansShows = array('Star Trek', 'Star Wars', 'Office Space', 'Simpsons');
+	$showsToWatch = array();
+	$moviesToAvoid = array();
+
+	for($x = 0; $x < $dylansShows; $x++) {
+		if($x === in_array("Star Wars", $dylansShows)) {
+			array_push($moviesToAvoid, $dylansShows[$x]);
+			return $moviesToAvoid;
+		} else {
+			array_push($showsToWatch, $dylansShows[$x]);
+			return $showsToWatch;
+		}
+	}
+	var_dump($showsToWatch);
+};
+
+shows();
