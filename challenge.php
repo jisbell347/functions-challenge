acb635045836a386237f2abf91a58b08ca308f60
Joseph Isbell <shadowwolf347@comcast.net>
2018-07-23 09:59:53 -0600

    Fixed array function to push shows into proper array

--- challenge.php
+++ challenge.php
@@ -1,11 +1,12 @@
 <?php
-/**
- * @params
- * @return
- **/
-// Math Dhallenge in PHP
 
-//Takes two random numbers between 1-6 and adds them together then returns the sum;
+// Math Challenge in PHP
+/**
+ * Takes two random numbers between 1-6 and adds them together then returns the sum;
+ *
+ * @params int $dice a random number chosen by the computer between 1 and 6
+ * @return int sum of two random numbers
+ **/
 function diceRoll() {
 	$dice = rand(1,6) + rand(1,6);
 	return "You rolled a $dice!";
@@ -14,6 +15,13 @@ function diceRoll() {
 echo diceRoll();
 
 // String Challenge in PHP
+/**
+ * Takes a parameter and checks if it equals any of the set strings and returns the output. If not, returns a default response
+ *
+ * @params string $animalName a string of an animal name
+ * @return string returns a preset string if the parameter matches the input string. Else, gives default response.
+ *
+ **/
 function animals($animalName) {
 	if($animalName === "Rabbit")
 		return "$animalName is written ウサギ in Japanese.";
@@ -28,22 +36,29 @@ function animals($animalName) {
 echo animals("Cat");
 
 // Array Challenge in PHP
+/**
+ * Takes a parameter and checks if it equals any of the set strings and returns the output. If not, returns a default response
+ *
+ * @params string $animalName a string of an animal name
+ * @return string returns a preset string if the parameter matches the input string. Else, gives default response.
+ *
+ **/
 function shows() {
 	$dylansShows = array('Star Trek', 'Star Wars', 'Office Space', 'Simpsons');
 	$showsToWatch = array();
 	$moviesToAvoid = array();
 
 	for($x = 0; $x < count($dylansShows); $x++) {
-		if(in_array("Star Wars", $dylansShows)) {
-			array_push($moviesToAvoid, $dylansShows[$x]);
+		if($dylansShows[$x] === "Star Wars") {
+			$moviesToAvoid[] = $dylansShows[$x];
 		} else {
-			array_push($showsToWatch, $dylansShows[$x]);
+			$showsToWatch[] = $dylansShows[$x];
 		}
 	}
 	$avoid = implode(',', $moviesToAvoid);
 	$watch = implode(' ,', $showsToWatch);
 
-	echo "The shows to avoid are $avoid and the shows to watch are $watch.";
+	return "The shows to avoid are" . $avoid . "and the shows to watch are " . $watch;
 };
 
-shows();
+echo shows();
